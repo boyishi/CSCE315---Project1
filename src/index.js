02ac4a4413e330188476270092b9e347f731c88b
Boyi Shi <riceboishi@tamu.edu>
2021-01-28 17:09:15 -0600

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import $ from "jquery";
+import Popper from "popper.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import App from "./App";
 
